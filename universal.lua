a49063685e61537828438c343175679e9b47d126
killxistcool <141137008+killxistcool@users.noreply.github.com>
2026-05-14 18:34:42 +0200

    Update loader version and add Murder Mystery 2 script
    Updated the loader version and added a new game script for Murder Mystery 2.

--- universal.lua
+++ universal.lua
@@ -1,5 +1,5 @@
 local gameId = game.GameId
-local version = "1.1"
+local version = "1.2"
 print("=== Loader version: " .. version .. " ===")
 
 if gameId == 2142948266 then
@@ -9,6 +9,10 @@ if gameId == 2142948266 then
 elseif gameId == 7436755782 then
     print("Loading Grow a Garden script...")
     loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/66758918b9c0087a3003138b6042d7bcbebdab3ba8794df8ad270065dc1f9ab7/download"))()
+
+elseif gameId = 66654135 then
+    print("Loading Murder Mystery 2 script..."
+    loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/87080c7b257bf5823113465709a972726ec7604afb5b33faa8ecd3343f9a957d/download"))()
 
 else
     game.Players.LocalPlayer:Kick("No script found for this game!")
